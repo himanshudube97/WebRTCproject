31482c1a87b0fc70cf4891bb7136911994426b1d
himanshudube97 <himanshu.dube13@gmail.com>
2023-09-13 14:04:22 +0530

    second part finished

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import styles from "./Login.module.css";
+import { StepPhoneEmail } from "../Steps/StepPhoneEmail/StepPhoneEmail";
+import { StepOtp } from "../Steps/StepOtp/StepOtp";
+const steps: { [key: number]: (props: { onNext: () => void }) => JSX.Element } =
+  {
+    1: StepPhoneEmail,
+    2: StepOtp,
+  };
+
+export const Login = () => {
+  const [step, setStep] = useState(1);
+  const Step = steps[step];
+  const onNext = () => {
+    setStep(step + 1);
+  };
+  return (
+    <>
+      <div>Login</div>
+      <Step onNext={onNext} />
+    </>
+  );
+};
